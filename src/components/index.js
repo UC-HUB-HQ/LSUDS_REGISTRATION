a4c12f17886e80bc62bf1a6757adb421af18352b
okondev2021 <sojapril16@gmail.com>
2024-08-20 02:01:58 +0100

    removed form.txt import

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
 export {default as LsudsInterestForm } from './LsudsInterestForm'
 export {default as ApplicantInfoForm } from './ApplicantInfoForm'
-
 export {default as FormProgress } from './FormProgress'
-export {default as Form} from './form'
