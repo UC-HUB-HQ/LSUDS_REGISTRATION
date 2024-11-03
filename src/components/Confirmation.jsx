a852fcb81e6cfb7486fe84bdb5e1bad20c435434
okondev2021 <sojapril16@gmail.com>
2024-11-03 20:06:37 +0100

    added uchub

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,8 +1,5 @@
 const Confirmation = () => {
 
-    const refresh = () => {
-        location.reload()
-    }
     return (
         <section className="flex flex-col items-center justify-center gap-6 text-center">
             <div className=" flex justify-center items-center w-[200px] aspect-square border border-blue rounded-full">
@@ -12,7 +9,7 @@ const Confirmation = () => {
                 <h2 className="text-4xl font-bold text-blue">Submission Successful</h2>
                 <p className=" text-grayishBlue">Thank you for expressing your interest in joining the LASU Debate Society. Your application has been successfully submitted. Please check your email for a confirmation message, and ensure to review your spam or junk folder if it does not appear in your inbox. We appreciate your enthusiasm and look forward to thoroughly reviewing your application.</p>
             </div>
-            <button className="px-4 py-3 text-white rounded-md bg-customred" onClick={refresh}>Submit Another Response</button>
+            <button className="px-4 py-3 text-white rounded-md bg-customred"><a href="https://www.theuchub.com/">Application created by UCHUB</a></button>
         </section>
     )
 }
